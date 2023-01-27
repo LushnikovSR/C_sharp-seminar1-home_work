2fa9e1bff388aefa428523bdec46a98164a73d5b
Stanislav_Lushnikov <lust48h@gmail.com>
2023-01-27 22:17:42 +0300

    add homework task 005

---
+++ HW_task005/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача № 2
+// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+// a = 5; b = 7 ->  max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+double number1 = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double number2 = double.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"max = {number1}");
+}
+else if (number2 > number1)
+{
+    Console.WriteLine($"max = {number2}");
+}
+else
+{
+    Console.WriteLine("Введенные числа равны.");
+}
